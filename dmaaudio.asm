d0b62592f0706e8ce8a452de046dde2d8d3e286c
DananaPro <156544657+DananaPro@users.noreply.github.com>
2026-05-21 23:03:29 +0300

    formated

--- dmaaudio.asm
+++ dmaaudio.asm
@@ -1,22 +1,26 @@
-; ========================================================
+; ==============================================================================
 ; dmaaudio.asm - TASM 4.1 Bulletproof Streaming Audio
-; ========================================================
+; Uses DMA (Direct Memory Access) and IRQ interrupts for background playback.
+; ==============================================================================
 
 DATASEG
     audioFileHandle  dw 0
-    currentAudioName dw 0    ; Saves the file name for recovery
+    currentAudioName dw 0    ; Saves the file name for re-opening if 'showbmp' steals the handle
     audioBufferSeg   dw 0
     audioBufferOff   dw 0
     oldIRQSeg        dw 0
     oldIRQOff        dw 0
-    activeHalf       db 0  
-    needsFill        db 0  
+    activeHalf       db 0    ; Tracks which half of the DMA buffer to fill next
+    needsFill        db 0    ; Flag set by IRQ to trigger a buffer refill
     
-    bufSize          equ 8192 
-    halfSize         equ 4096 
+    bufSize          equ 8192 ; Total DMA buffer size
+    halfSize         equ 4096 ; Half-size for ping-pong buffering
 
 CODESEG
 
+; ------------------------------------------------------------------------------
+; Send a command byte to the Sound Blaster DSP (Port 22Ch)
+; ------------------------------------------------------------------------------
 proc writeDSP
     push dx
     push ax
@@ -24,25 +28,29 @@ proc writeDSP
 wait_write:
     in al, dx
     test al, 80h
-    jnz wait_write
+    jnz wait_write           ; Wait until DSP is ready to accept commands
     pop ax
     out dx, al
     pop dx
     ret
 endp writeDSP
 
+; ------------------------------------------------------------------------------
+; Allocate memory for the audio buffer and align it to a 4KB boundary
+; ------------------------------------------------------------------------------
 proc initAudioSystem
     mov ah, 48h
-    mov bx, 1024      
+    mov bx, 1024             ; Request 16KB (1024 paragraphs)
     int 21h
     mov [audioBufferSeg], ax
     mov [audioBufferOff], 0
 
+    ; Ensure 4KB alignment (Required for DMA to not cross page boundaries)
     xor eax, eax
     mov ax, [audioBufferSeg]
-    shl eax, 4        
+    shl eax, 4
     mov ebx, eax
-    add ebx, 8191     
+    add ebx, 8191
     shr eax, 16
     shr ebx, 16
     cmp eax, ebx
@@ -59,6 +67,10 @@ safe_aligned:
     ret
 endp initAudioSystem
 
+; ------------------------------------------------------------------------------
+; Sound Blaster Interrupt Handler (IRQ)
+; Triggers whenever the DMA finishes playing a half-buffer
+; ------------------------------------------------------------------------------
 proc sb_irq_handler
     push ax
     push dx
@@ -67,14 +79,14 @@ proc sb_irq_handler
     mov ax, @data
     mov ds, ax
 
-    mov dx, 22Eh
+    mov dx, 22Eh             ; Acknowledge the interrupt to the DSP
     in al, dx
 
-    mov al, 20h
+    mov al, 20h              ; Send EOI (End Of Interrupt) to PIC
     out 20h, al
 
-    xor [activeHalf], 1
-    mov [needsFill], 1
+    xor [activeHalf], 1      ; Flip the ping-pong buffer flag
+    mov [needsFill], 1       ; Signal the main loop to fill the empty half
 
     pop ds
     pop dx
@@ -82,36 +94,38 @@ proc sb_irq_handler
     iret
 endp sb_irq_handler
 
+; ------------------------------------------------------------------------------
+; Open an audio file and initialize DMA transfer
+; ------------------------------------------------------------------------------
 proc playStreamingAudio
-    mov [currentAudioName], dx  
+    mov [currentAudioName], dx
 
-    ; --- PROPER CLEANUP ---
-    ; Only stop the specific audio handle, do NOT sweep-close all handles
-    call stopStreamingAudio 
+    ; Cleanup any currently playing audio
+    call stopStreamingAudio
 
-    ; ---------------------------------------------------------------
-
+    ; Open File
     mov ax, 3D00h
     mov dx, [currentAudioName]
     int 21h
     mov [audioFileHandle], ax
 
+    ; Pre-fill buffer with initial data
     mov bx, [audioFileHandle]
     mov dx, [audioBufferOff]
     mov cx, bufSize
-    
     push ds
     mov ds, [audioBufferSeg]
     mov ah, 3Fh
     int 21h
     pop ds
 
+    ; Hook the IRQ interrupt
     mov ax, 350Fh
     int 21h
     mov [oldIRQSeg], es
     mov [oldIRQOff], bx
 
-    cli                 ; Lock CPU to prevent crash while hooking
+    cli                      ; Disable interrupts while hooking
     push ds
     mov ax, cs
     mov ds, ax
@@ -119,31 +133,33 @@ proc playStreamingAudio
     mov ax, 250Fh
     int 21h
     pop ds
-    sti                 ; Unlock CPU
+    sti                      ; Re-enable interrupts
 
+    ; Enable Sound Blaster IRQ
     in al, 21h
     and al, 07Fh
     out 21h, al
 
+    ; Setup DMA Channel 1 for transfer
     mov dx, 0Ah
-    mov al, 05h
+    mov al, 05h              ; Mask DMA channel 1
     out dx, al
 
     mov dx, 0Ch
-    mov al, 00h
+    mov al, 00h              ; Reset DMA flip-flop
     out dx, al
 
     mov dx, 0Bh
-    mov al, 59h       
+    mov al, 59h              ; Mode: Single, Write, Ch 1
     out dx, al
 
+    ; Set DMA Memory Address
     xor eax, eax
     mov ax, [audioBufferSeg]
     shl eax, 4
     xor ebx, ebx
     mov bx, [audioBufferOff]
     add eax, ebx
-
     mov dx, 02h
     out dx, al
     mov al, ah
@@ -152,6 +168,7 @@ proc playStreamingAudio
     mov dx, 83h
     out dx, al
 
+    ; Set DMA Transfer Count
     mov ax, bufSize
     dec ax
     mov dx, 03h
@@ -160,86 +177,90 @@ proc playStreamingAudio
     out dx, al
 
     mov dx, 0Ah
-    mov al, 01h
+    mov al, 01h              ; Unmask DMA channel 1
     out dx, al
 
-    mov al, 0D1h      
+    ; Initialize DSP
+    mov al, 0D1h
     call writeDSP
-    mov al, 40h       
+    mov al, 40h
     call writeDSP
-    mov al, 131       
+    mov al, 131              ; Frequency setting (Adjustable)
     call writeDSP
     
-    mov al, 48h       
+    mov al, 48h              ; Set Block Size
     call writeDSP
     mov ax, halfSize
-    dec ax            
+    dec ax
     call writeDSP
     mov al, ah
     call writeDSP
 
-    mov al, 1Ch       
+    mov al, 1Ch              ; Start 8-bit Auto-Init DMA Transfer
     call writeDSP
 
     ret
 endp playStreamingAudio
 
+; ------------------------------------------------------------------------------
+; Called every frame to keep the audio buffer topped up
+; ------------------------------------------------------------------------------
 proc pollAudio
-    ; If no file is playing, return immediately to keep the loop moving
     cmp [audioFileHandle], 0
     je @done
-    pusha                 ; Protect the main game's math from corruption!
+    pusha                    ; Protect main game math
     
     cmp [needsFill], 1
     jne pollDone
     mov [needsFill], 0
 
+    ; Calculate memory offset for the inactive buffer half
     xor bx, bx
     mov bl, [activeHalf]
     xor bx, 1
     mov ax, halfSize
     mul bx
     add ax, [audioBufferOff]
-    mov dx, ax        
+    mov dx, ax
 
     mov bx, [audioFileHandle]
     mov cx, halfSize
 
+    ; Read next chunk from file
     push ds
     mov ds, [audioBufferSeg]
     mov ah, 3Fh
     int 21h
     pop ds
 
-    jc file_error         ; If showbmp closed our file, jump to recovery!
+    jc file_error            ; Self-Healing: If file was stolen, recover
 
     cmp ax, halfSize
-    je pollDone
+    je pollDone              ; Successfully filled chunk
 
-    push ax           
+    ; If we reached end of file, restart file playback
+    push ax
     mov ax, 4200h
     mov bx, [audioFileHandle]
     xor cx, cx
     xor dx, dx
     int 21h
-
     pop ax
+
+    ; Fill remainder of buffer from the start of the file
     mov cx, halfSize
-    sub cx, ax        
-    
+    sub cx, ax
     xor dx, dx
     mov dl, [activeHalf]
     xor dx, 1
-    push ax           
+    push ax
     mov ax, halfSize
     mul dx
-    pop dx            
-    add ax, dx        
+    pop dx
+    add ax, dx
     add ax, [audioBufferOff]
     mov dx, ax
-
     mov bx, [audioFileHandle]
-    
     push ds
     mov ds, [audioBufferSeg]
     mov ah, 3Fh
@@ -248,7 +269,6 @@ proc pollAudio
     jmp pollDone
 
 file_error:
-    ; Self-Healing: Re-open the file silently if it was stolen by showbmp
     mov ax, 3D00h
     mov dx, [currentAudioName]
     int 21h
@@ -260,27 +280,24 @@ pollDone:
     ret
 endp pollAudio
 
+; ------------------------------------------------------------------------------
+; Stop all audio and close file handles
+; ------------------------------------------------------------------------------
 proc stopStreamingAudio
     cmp [audioFileHandle], 0
     je skipStop
 
-    mov al, 0DAh
+    mov al, 0DAh             ; DMA Stop
     call writeDSP
-    mov al, 0D3h
+    mov al, 0D3h             ; DSP Stop
     call writeDSP
 
-    ; --------------------------------------------------------
-    ; WE DELETED THE 'cli' TO 'sti' UNHOOKING BLOCK HERE!
-    ; We must leave the handler attached so it safely absorbs
-    ; the final delayed interrupt from the Sound Blaster.
-    ; --------------------------------------------------------
-
-    mov ah, 3Eh
+    mov ah, 3Eh              ; Close file
     mov bx, [audioFileHandle]
     int 21h
     
     mov [audioFileHandle], 0
-    mov [needsFill], 0      ; Clear any pending buffer fills!
+    mov [needsFill], 0
 skipStop:
     ret
 endp stopStreamingAudio
